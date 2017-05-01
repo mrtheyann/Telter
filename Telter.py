694f5796f9117ce65280b80b87aa7831311b373a
mrtheyann <darklodr@gmail.com>
2017-05-01 21:45:18 +0600

    upd: html markup

--- Telter.py
+++ Telter.py
@@ -16,7 +16,7 @@ from prompt_toolkit import prompt
 Twitter OAuth data
 '''
 
-access_token = 'access_token '
+access_token = 'access_token'
 access_secret = 'access_secret'
 consumer_key = 'consumer_key'
 consumer_secret = 'consumer_secret'
@@ -34,6 +34,35 @@ api = twitter.Api(consumer_key = consumer_key,
 
 user = api.VerifyCredentials(include_entities=False, skip_status=True, include_email=False)
 
+def confirm_mode():
+  html = set(['html', 'h', 'HTML', 'H'])
+  markdown = set(['markdown', 'm', 'md', 'MD'])
+
+  while True:
+    mode = input('> ').lower()
+    if mode in markdown:
+      return True
+    elif mode in html:
+      return False
+    else:
+      print("Please, respond with 'html' or 'markdown'")
+
+
+#Creating telegraph post with markdown formatting
+def new_post(title, content, mode):
+  if mode is True:
+    content = markdown(content, output_format='xhtml5')
+
+  telegraph.create_account(short_name=user.screen_name)
+  response = telegraph.create_page(
+    author_name='{0} | @{1}'.format(user.name, user.screen_name),
+    title=title,
+    html_content=content
+    )
+  
+  link = response['path']
+  print(content)
+  return 'http://telegra.ph/{}'.format(link)
 
 #Updating twitter status
 def update_status(url):
@@ -42,29 +71,24 @@ def update_status(url):
   sys.exit()
   pass
 
-#Creating telegraph post
-def new_post(title, content):
-  telegraph.create_account(short_name=user.screen_name)
-  response = telegraph.create_page(
-    author_name='{0} | @{1}'.format(user.name, user.screen_name),
-    title=title,
-    html_content=markdown(content, output_format='xhtml5')
-    )
-  link = response['path']
-  print('http://telegra.ph/{}'.format(link))
-  return link
-
-
 #Main controller
 def main():
   print('Enter the title of your article:')
   title = prompt('> ')
+  
+  print('Enter the markup format (html/markdown):')
+  mode = confirm_mode()
+  print(mode)
+
   print('Enter the content of your article.' '\n'
    'Once done, press Meta+Enter (Or Escape followed by Enter) in order to accept the input.')
 
+
   content = prompt('> ', multiline=True) 
-  post = new_post(title, content)
-  update_status('http://telegra.ph/{}'.format(post))
+  post = new_post(title, content, mode)
+
+  print(post)
+  update_status(post)
 
 if __name__ == '__main__':
   main()
